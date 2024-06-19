c390795818c1442167305f32e2c8e33adbec9e6e
elxupa <enzocprd@gmail.com>
2024-06-19 21:33:22 +0000

    Mundo guardado 2024-06-19

--- servidor.py
+++ servidor.py
@@ -46,7 +46,7 @@ def download_latest_release(download_path='.'):
 		version = url.split("/")[-1]
 		pathto = os.path.join(download_path, version)
 		with open(pathto, 'wb') as archivo:
-			archivo.write(requests.get(url).content
+			archivo.write(requests.get(url).content)
 		return version
 flnm=download_latest_release()
 if flnm.split(".")[-1] == "pyc":
